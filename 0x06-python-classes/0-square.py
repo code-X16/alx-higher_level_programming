ef00c30950ed1d9d5f154236ffdfd631458bd0f2
Bakare Opeyemi <bakareopeyemi1722@gmail.com>
2023-08-29 10:46:12 -0700

    Added task

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -4,5 +4,5 @@
 
 
 class Square:
-
+    """ empty square """
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,12 +3,14 @@
 
 
 class Square:
-    def __init__(self, size=0):
-
-        if type(size) is int:
-            if size < 0:
-                raise ValueError('size must be >= 0')
-            else:
-                self.__size = size
-            else:
-                raise TypeError('size must be an integer')
+    """Represents a square
+    Attributes:
+        __size (int): size of a side of the square
+    """
+    def __init__(self, size):
+        """Initializes a square
+        Args:
+            size (int): size of a side of the square
+        Returns: None
+        """
+        self.__size = size
