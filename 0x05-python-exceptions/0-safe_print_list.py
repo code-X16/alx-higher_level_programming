bf8b2eb85ec791d716af591e5839f36b07d4d203
Bakare Opeyemi <bakareopeyemi1722@gmail.com>
2023-08-28 19:02:31 -0700

    Added task

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    for m in range(x):
+        try:
+            print(my_list[m], end="")
+            i += 1
+        except IndexError:
+            break
+        print()
+        return i
